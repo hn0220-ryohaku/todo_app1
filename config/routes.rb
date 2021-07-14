8f92dc162cc486094ac08e3b23a5b9e1b521d130
hn0220-ryohaku <anty.meg.perfume@gmail.com>
2021-07-14 11:00:24 +0000

    finish 2-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #home_controllerのindexメソッドを割り当てる
+  root to: "home#index"
 end
