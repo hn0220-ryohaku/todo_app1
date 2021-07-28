002716a23c1eb2c22c0e777cb5d58822799f1a29
hn0220-ryohaku <anty.meg.perfume@gmail.com>
2021-07-28 00:20:29 +0000

    finish 2-12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_214606) do
+ActiveRecord::Schema.define(version: 2021_07_21_093513) do
 
   create_table "goals", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_07_15_214606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "todos", force: :cascade do |t|
